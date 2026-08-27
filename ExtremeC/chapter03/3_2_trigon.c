b78265c00e24e3205bd5bc19cc14e7de611b64f1
Yush1nk1m <kys010306@sogang.ac.kr>
2026-08-27 15:03:40 +0900

    Chapter03 예제 코드 추가
    - [3-2], [3-3]

---
+++ ExtremeC/chapter03/3_2_trigon.c
@@ -0,0 +1,28 @@
+#include <math.h>
+
+// include user-defined header file to use macro PI
+#include "3_2_geometry.h"
+
+double to_radian(double deg) {
+    return (PI * deg) / 180;
+}
+
+double to_degree(double rad) {
+    return (180 * rad) / PI;
+}
+
+double cos_deg(double deg) {
+    return cos(to_radian(deg));
+}
+
+double acos_deg(double deg) {
+    return acos(to_radian(deg));
+}
+
+double sin_deg(double deg) {
+    return sin(to_radian(deg));
+}
+
+double asin_deg(double deg) {
+    return asin(to_radian(deg));
+}
