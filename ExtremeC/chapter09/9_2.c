2cbdbaf9f0e5e2982fa2f39d13352f77a03e9b29
Yush1nk1m <kys010306@sogang.ac.kr>
2026-09-06 01:07:57 +0900

    Chapter09 예제 코드 작성
    - [9-1], [9-2], [9-3]

---
+++ ExtremeC/chapter09/9_2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+typedef struct {
+    char c;
+    char d;
+} a_t;
+
+typedef struct {
+    a_t parent;
+    char str[5];
+} b_t;
+
+int main(int argc, char** argv) {
+    b_t b;
+    b.parent.c = 'A';
+    b.parent.d = 'B';
+    strcpy(b.str, "1234");
+    // breakpoint here
+    return 0;
+}
---
+++ ExtremeC/chapter09/9_2.cpp
@@ -0,0 +1,21 @@
+#include <string.h>
+
+class A {
+public:
+    char c;
+    char d;
+};
+
+class B : public A {
+public:
+    char str[5];
+};
+
+int main(int argc, char** argv) {
+    B b;
+    b.c = 'A';
+    b.d = 'B';
+    strcpy(b.str, "1234");
+    // breakpoint here
+    return 0;
+}
